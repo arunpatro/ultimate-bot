7c2d86765df503158295f0381e1d0df4b4d5b719
Arun Patro <arun.patro@myntra.com>
2018-10-23 17:21:22 +0530

    make functional game

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,36 @@ from app import app
 from flask import request, jsonify, json
 from glob import glob
 import random
+import library as lib
 
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
+ttt = lib.TicTacToe()
+ttt.reset()
+
 @app.route('/')
 def index():
+    ttt.reset()
     return render_template('index.html')
+
+@app.route('/reset')
+def reset():
+    ttt.reset()
+    return jsonify(False)
+
+@app.route('/play', methods=['GET', 'POST'])
+def play():
+    data=None
+    if request.method == "POST":
+        data = json.loads(request.get_data())
+    move = int(data['move'][-1])
+    player = int(data['player'])
+    position = lib.MOVES[move]
+    ttt.play(position)
+    print('Player {} and move {}'.format(player, move))
+    print(ttt.board)
+    wp = lib.who_win(ttt)
+    out = {
+        'wp': wp
+    }
+    return jsonify(out)
--- library.py
+++ library.py
@@ -3,6 +3,17 @@ import numpy as np
 import time
 import random
 
+MOVES = {
+    0: (0, 0),
+    1: (0, 1),
+    2: (0, 2),
+    3: (1, 0),
+    4: (1, 1),
+    5: (1, 2),
+    6: (2, 0),
+    7: (2, 1),
+    8: (2, 2),
+}
 class TicTacToe:
     def __init__(self, board = np.zeros((3,3)), player = 1):
         self.board = board
